c5c7306d9a8b169c4a98ddcbce98875d87a617a4
Speefictor <gongcunji@gmail.com>
2015-08-27 19:18:49 +0800

    Finish Publish and Subscribe
    Finish Publish and Subscribe

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,101 @@
+Tasks=new Mongo.Collection('tasks');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    
+    Meteor.subscribe('tasks');
+    
+    Template.body.helpers({
+        tasks:function(){
+            if(Session.get('hideCompleted')){
+                return Tasks.find({checked:{$ne:true}},{sort:{createAt:-1}});
+            }else{
+                
+                return Tasks.find({},{sort:{createAt:-1}});
+            }
+            
+        },
+        hideCompleted:Session.get('hideCompleted'),
+        incompleteCount: function(){
+            return Tasks.find({checked:{$ne:true}}).count();
+        }
+        /*[
+            {text:'This is task 1'},
+            {text:'This is task 2'},
+            {text:'This is task 3'},
+            {text:'This is task 4'},
+        ]*/
+    });
+    
+    Template.body.events({
+        'submit .new-task':function(event){
+            event.preventDefault();
+            
+            var text=event.target.text.value;
+            
+            Meteor.call('addTask',text);
+            
+            event.target.text.value='';
+        },
+        'change .hide-completed input':function(event,template){
+            Session.set("hideCompleted",event.target.checked);
+        }
+    });
+    
+    Template.task.events({
+        'click .toggle-checked':function(event,template){
+            //Tasks.update(this._id,{$set:{checked:!this.checked}});
+            Meteor.call('setChecked',this._id, !this.checked);
+        },
+        'click .delete':function(event,template){
+            //Tasks.remove(this._id);
+            Meteor.call("deleteTask",this._id);
+        }
+    });
+    
+    
+    Accounts.ui.config ({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+    
+    
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+ 
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+    
+    Meteor.publish("tasks",function(){
+      /*  if(Session.get('hideCompleted')){
+                return Tasks.find({checked:{$ne:true}},{sort:{createAt:-1}});
+            }else{
+             */   
+                return Tasks.find({},{sort:{createAt:-1}});
+           // }
+    });
+    
+    Meteor.methods({
+        addTask:function(text){
+            if(!Meteor.userId()){
+                throw new Meteor.Error("not-authorized");
+            }
+            Tasks.insert({
+                text:text,
+                owner:Meteor.userId(),
+                userName:Meteor.user().username,
+                createAt: new Date()
+            });
+            
+        },
+        
+        deleteTask:function(taskId){
+            Tasks.remove(taskId);
+        },
+        
+        setChecked:function(taskId, setChecked ){
+            Tasks.update(taskId,{$set:{checked:setChecked }});   
+        }
+    });
 }
